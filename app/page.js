61fb31dd12dbe9a8457fbfe975c6b32a6acbe317
Mohammed <sajidcurious9@gmail.com>
2023-08-14 23:31:12 +0530

    building page.js

--- app/page.js
+++ app/page.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="products-heading">
-      <div>Home page</div>
-    </div>
+    <>
+      HeroBanner
+      <div className="products-heading">
+        <div>Home page</div>
+      </div>
+    </>
   );
 };
 
