326bf421a7b7bba815bfbc37ad8a8b516851e833
Mohammed <sajidcurious9@gmail.com>
2023-08-14 23:50:35 +0530

    imported all the components to app.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
 import React from "react";
+import HeroBanner from "@/components/HeroBanner";
+import Cart from "@/components/Cart";
+import Footer from "@/components/Footer";
+import FooterBanner from "@/components/FooterBanner";
+import Layout from "@/components/Layout";
+import Navbar from "@/components/Navbar";
+import Product from "@/components/Product";
 
 const page = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className="products-heading">
         <div>Home page</div>
       </div>
