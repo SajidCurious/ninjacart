25e2ba6fd25a18e1c3c16cced384b5fc2b2e8c24
Mohammed <sajidcurious9@gmail.com>
2023-08-15 22:59:08 +0530

    importing the data from client to page.js to import data to connect with sanity

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import FooterBanner from "@/components/FooterBanner";
 import Layout from "@/components/Layout";
 import Navbar from "@/components/Navbar";
 import Product from "@/components/Product";
+import client from "@/lib/client";
 
 const page = () => {
   return (
@@ -14,8 +15,21 @@ const page = () => {
       <div className="products-heading">
         <div>Home page</div>
       </div>
+      <Footer />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default page;
